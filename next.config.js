46dd683875f96c8cf4d5bdae1e7b1e0f1e502ded
nino-space <takuya.ninoseki@seikei-gakushu.com>
2026-03-22 23:07:41 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-/** @type {import('next').NextConfig} */
+js/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
